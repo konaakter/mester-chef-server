109c7d90837c2b40d6f7846449539bee7c59fe1e
unknown <kona>
2023-05-02 15:58:18 +0600

    hello run

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
